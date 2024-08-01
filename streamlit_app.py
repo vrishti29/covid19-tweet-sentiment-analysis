e085a68834d95b8fcec7af6a33599e71e569cef5
Vrishti Garg <105159048+vrishti29@users.noreply.github.com>
2024-08-01 13:13:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,10 @@ st.title('Machine Learning App')
 
 st.info('This app builds a machine learnign Model')
 
-try: 
-  df = pd.read_csv("https://raw.githubusercontent.com/vrishti29/machineslearning/master/data/Coronavirus_Tweets.csv", encoding='utf-8')
-except UnicodeDecodeError:
-  df = pd.read_csv("https://raw.githubusercontent.com/vrishti29/machineslearning/master/data/Coronavirus_Tweets.csv", encoding='latin1')
+with st.expander('Data'):
+  try: 
+    df = pd.read_csv("https://raw.githubusercontent.com/vrishti29/machineslearning/master/data/Coronavirus_Tweets.csv", encoding='utf-8')
+  except UnicodeDecodeError:
+    df = pd.read_csv("https://raw.githubusercontent.com/vrishti29/machineslearning/master/data/Coronavirus_Tweets.csv", encoding='latin1')
 
-df
+  df

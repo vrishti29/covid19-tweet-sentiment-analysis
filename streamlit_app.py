1d7c81d5df7b041caab5749de849a734e0ac4564
Vrishti Garg <105159048+vrishti29@users.noreply.github.com>
2024-08-01 12:33:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Machine Learning App')
 
 st.info('This app builds a machine learnign Model')
+
+df = pd.read_csv("https://raw.githubusercontent.com/vrishti29/machineslearning/master/data/Coronavirus_Tweets.csv")
+df
